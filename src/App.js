f9edb490172c15dae9d6d1b9460778066d9aa31d
Sini Sandholm <sini.tuulia@hotmail.com>
2021-12-13 23:07:59 +0200

    Created basic files for the project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+import App from './App.css';
+
+function App() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <h2>
+            Car Fuel Expense Tracker and Calculator
+        </h2>
+    )
+}
