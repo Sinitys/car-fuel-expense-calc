096e06639b6ae946f2de39b6f84969daa5dc16ba
Sini Sandholm <sini.tuulia@hotmail.com>
2021-12-13 23:17:51 +0200

    Added header to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import reactDOM from 'react-dom';
+import { Header } from './components/Header';
 import App from './App.css';
 
 function App() {
     return (
         <div>
-            
+            <Header />
         </div>
     );
 }
